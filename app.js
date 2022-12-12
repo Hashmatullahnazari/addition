1d7ff9df522637b5b4af5958bdc939db8bd55686
Hashmatullah Nazari <hn0153@gmail.com>
2022-12-12 08:01:17 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var correctAnswer = 4;
+
+  // Define a function to check the answer
+  function checkAnswer() {
+    // Get the user's answer from the input field
+    var userAnswer = parseInt(document.getElementById("userAnswer").value);
+
+    // Check the answer and show a message
+    if (userAnswer == correctAnswer) {
+      var result = "You got it right! Good job!";
+    } else {
+     var result = "Sorry, that's not the correct answer. The correct answer is " + correctAnswer;
+    }
+      document.getElementById("result").innerHTML = result;
+  }
